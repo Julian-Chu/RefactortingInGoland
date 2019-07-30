33e829ec97c1b133ca4d47ad668566e310e1669a
yu-lang.chu <yu-lang.chu@redheads.de>
2019-07-30 17:11:11 +0800

    break lone line

--- demo3/demo3.go
+++ demo3/demo3.go
@@ -9,5 +9,7 @@ type Order struct {
 
 func Price(o Order) float64 {
 	// 基本價 - 數量折扣 + 運費
-	return math.Min(float64(o.Qty)*o.ItemPrice*0.1, float64(100)) - math.Max(0, float64(o.Qty-500))*o.ItemPrice*0.05 + float64(o.Qty)*o.ItemPrice
+	return math.Min(float64(o.Qty)*o.ItemPrice*0.1, float64(100)) -
+		math.Max(0, float64(o.Qty-500))*o.ItemPrice*0.05 +
+		float64(o.Qty)*o.ItemPrice
 }
