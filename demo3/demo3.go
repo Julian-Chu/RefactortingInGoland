5e8494be43e3e67c1dc4d4690da4b33be8e6a044
yu-lang.chu <yu-lang.chu@redheads.de>
2019-07-30 16:22:51 +0800

    change code order

--- demo3/demo3.go
+++ demo3/demo3.go
@@ -9,7 +9,5 @@ type Order struct {
 
 func Price(o Order) float64 {
 	// 基本價 - 數量折扣 + 運費
-
-	return float64(o.Qty)*o.ItemPrice - math.Max(0, float64(o.Qty-500))*o.ItemPrice*0.05 +
-		math.Min(float64(o.Qty)*o.ItemPrice*0.1, float64(100))
+	return math.Min(float64(o.Qty)*o.ItemPrice*0.1, float64(100)) - math.Max(0, float64(o.Qty-500))*o.ItemPrice*0.05 + float64(o.Qty)*o.ItemPrice
 }
